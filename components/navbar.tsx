43b83be24df854f8ff346aa9e0c432568b842d4a
Stephanie <stephaniiehallberg@gmail.com>
2023-07-06 09:19:32 +0200

    fix: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,6 @@ const Navbar = async () => {
 
   return (
     <div className="border-b">
-      Navbar
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
