3b529c36443c79492d8f4dfe6bc11bf6d1f2a882
Stephanie <stephaniiehallberg@gmail.com>
2023-07-06 09:12:51 +0200

    fix: product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,21 +1,10 @@
 "use client";
 
-/* import Currency from "@/components/ui/currency";
- */
 import { Expand, ShoppingCart } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { MouseEventHandler } from "react";
 
 import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
-
-/* import Currency from "@/components/ui/currency";
-import IconButton from "@/components/ui/icon-button"; */
-
-/* import useCart from "@/hooks/use-cart"; */
-
-/* import usePreviewModal from "@/hooks/use-preview-modal"; */
 import { Product } from "@/types";
 
 interface ProductCard {
@@ -23,29 +12,9 @@ interface ProductCard {
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
-  /*  const previewModal = usePreviewModal(); */
-  /*  const cart = useCart(); */
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/product/${data?.id}`);
-  };
-
-  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
-    event.stopPropagation();
-
-    /* previewModal.onOpen(data); */
-  };
-
-  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
-    event.stopPropagation();
-
-    /* cart.addItem(data); */
-  };
-
   return (
     <div
-      onClick={handleClick}
+      onClick={() => {}}
       className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
     >
       {/* Image & actions */}
@@ -59,11 +28,11 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={onPreview}
+              onClick={() => {}}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={onAddToCart}
+              onClick={() => {}}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
